3c6d4907e0767efbcb3b0ebe690e9d8a27a19562
sarfaraz4054 <sonuhyd0@gmail.com>
2025-03-28 16:50:40 +0530

    table re-organized

--- script.js
+++ script.js
@@ -1,67 +1,67 @@
 // Product data
 const products = [
-    { sno: 1, particulars: "PATTI A.0.5.0", gauge: "126", width: "171", price: "171" },
-    { sno: 2, particulars: "PATTI A.0.4.0", gauge: "126", width: "154", price: "154" },
-    { sno: 3, particulars: "PATTI B.0.5.0", gauge: "115", width: "133", price: "133" },
-    { sno: 4, particulars: "PATTI B.0.4.0", gauge: "115", width: "109", price: "109" },
-    { sno: 5, particulars: "PATTI B.0.3.5", gauge: "115", width: "99", price: "99" },
-    { sno: 6, particulars: "PATTI C.0.5.0", gauge: "105", width: "126", price: "126" },
-    { sno: 7, particulars: "PATTI C.0.4.0", gauge: "105", width: "101", price: "101" },
-    { sno: 8, particulars: "PATTI C.0.3.5", gauge: "105", width: "92", price: "92" },
-    { sno: 9, particulars: "PATTI C.0.3.0", gauge: "105", width: "80", price: "80" },
-    { sno: 10, particulars: "PATTI D.0.5.0", gauge: "101", width: "120", price: "120" },
-    { sno: 11, particulars: "PATTI D.0.4.0", gauge: "101", width: "96", price: "96" },
-    { sno: 12, particulars: "PATTI D.0.3.5", gauge: "101", width: "88", price: "88" },
-    { sno: 13, particulars: "PATTI D.0.3.0", gauge: "101", width: "78", price: "78" },
-    { sno: 14, particulars: "PATTI 0.3.0(Small)", gauge: "95", width: "74", price: "74" },
-    { sno: 15, particulars: "PATTI 0.0.0(Small)", gauge: "95", width: "66", price: "66" },
-    { sno: 16, particulars: "BOTTOM A.0.7.5", gauge: "82", width: "160", price: "160" },
-    { sno: 17, particulars: "BOTTOM A.0.6.0", gauge: "82", width: "122", price: "122" },
-    { sno: 18, particulars: "BOTTOM B.0.7.5", gauge: "72", width: "124", price: "124" },
-    { sno: 19, particulars: "BOTTOM B.0.6.0", gauge: "72", width: "101", price: "101" },
-    { sno: 20, particulars: "BOTTOM B.0.5.0", gauge: "72", width: "84", price: "84" },
-    { sno: 21, particulars: "BOTTOM C.0.7.5", gauge: "68", width: "117", price: "117" },
-    { sno: 22, particulars: "BOTTOM C.0.6.0", gauge: "65", width: "93", price: "93" },
-    { sno: 23, particulars: "BOTTOM C.0.5.0", gauge: "65", width: "81", price: "81" },
-    { sno: 24, particulars: "BOTTOM C.0.4.0", gauge: "65", width: "69", price: "69" },
-    { sno: 25, particulars: "BOTTOM 0.4.0(Small)", gauge: "61", width: "62", price: "62" },
-    { sno: 26, particulars: "BOTTOM 0.0.0(Small)", gauge: "61", width: "56", price: "56" },
-    { sno: 27, particulars: "PERIMETER A.5.0", gauge: "65", width: "96", price: "96" },
-    { sno: 28, particulars: "PERIMETER 0.5.0", gauge: "55", width: "69", price: "69" },
-    { sno: 29, particulars: "PERIMETER 0.4.0", gauge: "55", width: "55", price: "55" },
-    { sno: 30, particulars: "PERIMETER 0.3.5", gauge: "55", width: "50", price: "50" },
-    { sno: 31, particulars: "PERIMETER 0.3.0", gauge: "55", width: "47", price: "47" },
-    { sno: 32, particulars: "PERIMETER 0.0.0", gauge: "55", width: "42", price: "42" },
-    { sno: 33, particulars: "PERIMETER. 0.0.0", gauge: "50", width: "38", price: "38" },
-    { sno: 34, particulars: "PERIMETER.PL 0.3.5", gauge: "55", width: "52", price: "52" },
-    { sno: 35, particulars: "L.ANGLE 0.5.0", gauge: "35", width: "49", price: "49" },
-    { sno: 36, particulars: "L.ANGLE 0.4.0", gauge: "35", width: "39", price: "39" },
-    { sno: 37, particulars: "L.ANGLE 0.3.5", gauge: "35", width: "36", price: "36" },
-    { sno: 38, particulars: "L.ANGLE 0.3.0", gauge: "35", width: "32", price: "32" },
-    { sno: 39, particulars: "L.ANGLE 0.3.0", gauge: "32", width: "30", price: "30" },
-    { sno: 40, particulars: "L.ANGLE 0.0.0", gauge: "32", width: "27", price: "27" },
-    { sno: 41, particulars: "L.ANGLE.PL 0.3.5", gauge: "35", width: "41", price: "41" },
-    { sno: 42, particulars: "L.ANGLE.PL(8ft)", gauge: "35", width: "42", price: "30" },
-    { sno: 43, particulars: "PATT.TRUE.PL 0.4.0", gauge: "111", width: "112", price: "112" },
-    { sno: 44, particulars: "PATT.TRUE.PL 0.3.5", gauge: "111", width: "100", price: "100" },
-    { sno: 45, particulars: "BOTTOM.TRUE.PL 0.6.0", gauge: "68", width: "100", price: "100" },
-    { sno: 46, particulars: "BOTTOM.TRUE.PL 0.5.0", gauge: "68", width: "88", price: "88" },
-    { sno: 47, particulars: "STUD.3.inc.10.FT 0.5.0", gauge: "148", width: "173", price: "173" },
-    { sno: 48, particulars: "FLOOR.3.inc.10.FT 0.5.0", gauge: "124", width: "149", price: "149" },
-    { sno: 49, particulars: "STUD.3.inc.10.FT 0.4.0", gauge: "148", width: "147", price: "147" },
-    { sno: 50, particulars: "FLOOR.3.Inc.10.FT 0.4.0", gauge: "124", width: "126", price: "126" },
-    { sno: 51, particulars: "STUD.2.inc.10.FT 0.5.0", gauge: "124", width: "147", price: "147" },
-    { sno: 52, particulars: "FLOOR.2.inc.10.FT 0.5.0", gauge: "101", width: "123", price: "123" },
-    { sno: 53, particulars: "STUD.2.inc.10.FT 0.4.0", gauge: "124", width: "120", price: "120" },
-    { sno: 54, particulars: "FLOOR.2.inc.10.FT 0.4.0", gauge: "101", width: "101", price: "101" },
-    { sno: 55, particulars: "STUD.3.inc.12.FT 0.5.0", gauge: "148", width: "207", price: "207" },
-    { sno: 56, particulars: "FLOOR.3.inc.12.FT 0.5.0", gauge: "124", width: "179", price: "179" },
-    { sno: 57, particulars: "STUD.3.inc.12.FT 0.4.0", gauge: "148", width: "176", price: "176" },
-    { sno: 58, particulars: "FLOOR.3.Inc.12.FT 0.4.0", gauge: "124", width: "152", price: "152" },
-    { sno: 59, particulars: "STUD.2.inc.12.FT 0.5.0", gauge: "124", width: "177", price: "177" },
-    { sno: 60, particulars: "FLOOR.2.inc.12.FT 0.5.0", gauge: "101", width: "148", price: "148" },
-    { sno: 61, particulars: "STUD.2.inc.12.FT 0.4.0", gauge: "124", width: "144", price: "144" },
-    { sno: 62, particulars: "FLOOR.2.inc.12.FT 0.4.0", gauge: "101", width: "121", price: "121" }
+    { sno: 1, particulars: "PATTI", "gauge(mm)": "A.0.5.0", width: "126", price: "171" },
+    { sno: 2, particulars: "PATTI", "gauge(mm)": "A.0.4.0", width: "126", price: "154" },
+    { sno: 3, particulars: "PATTI", "gauge(mm)": "B.0.5.0", width: "115", price: "133" },
+    { sno: 4, particulars: "PATTI", "gauge(mm)": "B.0.4.0", width: "115", price: "109" },
+    { sno: 5, particulars: "PATTI", "gauge(mm)": "B.0.3.5", width: "115", price: "99" },
+    { sno: 6, particulars: "PATTI", "gauge(mm)": "C.0.5.0", width: "105", price: "126" },
+    { sno: 7, particulars: "PATTI", "gauge(mm)": "C.0.4.0", width: "105", price: "101" },
+    { sno: 8, particulars: "PATTI", "gauge(mm)": "C.0.3.5", width: "105", price: "92" },
+    { sno: 9, particulars: "PATTI", "gauge(mm)": "C.0.3.0", width: "105", price: "80" },
+    { sno: 10, particulars: "PATTI", "gauge(mm)": "D.0.5.0", width: "101", price: "120" },
+    { sno: 11, particulars: "PATTI", "gauge(mm)": "D.0.4.0", width: "101", price: "96" },
+    { sno: 12, particulars: "PATTI", "gauge(mm)": "D.0.3.5", width: "101", price: "88" },
+    { sno: 13, particulars: "PATTI", "gauge(mm)": "D.0.3.0", width: "101", price: "78" },
+    { sno: 14, particulars: "PATTI", "gauge(mm)": "0.3.0(Small)", width: "95", price: "74" },
+    { sno: 15, particulars: "PATTI", "gauge(mm)": "0.0.0(Small)", width: "95", price: "66" },
+    { sno: 16, particulars: "BOTTOM", "gauge(mm)": "A.0.7.5", width: "82", price: "160" },
+    { sno: 17, particulars: "BOTTOM", "gauge(mm)": "A.0.6.0", width: "82", price: "122" },
+    { sno: 18, particulars: "BOTTOM", "gauge(mm)": "B.0.7.5", width: "72", price: "124" },
+    { sno: 19, particulars: "BOTTOM", "gauge(mm)": "B.0.6.0", width: "72", price: "101" },
+    { sno: 20, particulars: "BOTTOM", "gauge(mm)": "B.0.5.0", width: "72", price: "84" },
+    { sno: 21, particulars: "BOTTOM", "gauge(mm)": "C.0.7.5", width: "68", price: "117" },
+    { sno: 22, particulars: "BOTTOM", "gauge(mm)": "C.0.6.0", width: "65", price: "93" },
+    { sno: 23, particulars: "BOTTOM", "gauge(mm)": "C.0.5.0", width: "65", price: "81" },
+    { sno: 24, particulars: "BOTTOM", "gauge(mm)": "C.0.4.0", width: "65", price: "69" },
+    { sno: 25, particulars: "BOTTOM", "gauge(mm)": "0.4.0(Small)", width: "61", price: "62" },
+    { sno: 26, particulars: "BOTTOM", "gauge(mm)": "0.0.0(Small)", width: "61", price: "56" },
+    { sno: 27, particulars: "PERIMETER", "gauge(mm)": "A.5.0", width: "65", price: "96" },
+    { sno: 28, particulars: "PERIMETER", "gauge(mm)": "0.5.0", width: "55", price: "69" },
+    { sno: 29, particulars: "PERIMETER", "gauge(mm)": "0.4.0", width: "55", price: "55" },
+    { sno: 30, particulars: "PERIMETER", "gauge(mm)": "0.3.5", width: "55", price: "50" },
+    { sno: 31, particulars: "PERIMETER", "gauge(mm)": "0.3.0", width: "55", price: "47" },
+    { sno: 32, particulars: "PERIMETER", "gauge(mm)": "0.0.0", width: "55", price: "42" },
+    { sno: 33, particulars: "PERIMETER", "gauge(mm)": "0.0.0", width: "50", price: "38" },
+    { sno: 34, particulars: "PERIMETER.PL", "gauge(mm)": "0.3.5", width: "55", price: "52" },
+    { sno: 35, particulars: "L.ANGLE", "gauge(mm)": "0.5.0", width: "35", price: "49" },
+    { sno: 36, particulars: "L.ANGLE", "gauge(mm)": "0.4.0", width: "35", price: "39" },
+    { sno: 37, particulars: "L.ANGLE", "gauge(mm)": "0.3.5", width: "35", price: "36" },
+    { sno: 38, particulars: "L.ANGLE", "gauge(mm)": "0.3.0", width: "35", price: "32" },
+    { sno: 39, particulars: "L.ANGLE", "gauge(mm)": "0.3.0", width: "32", price: "30" },
+    { sno: 40, particulars: "L.ANGLE", "gauge(mm)": "0.0.0", width: "32", price: "27" },
+    { sno: 41, particulars: "L.ANGLE.PL", "gauge(mm)": "0.3.5", width: "35", price: "41" },
+    { sno: 42, particulars: "L.ANGLE.PL(8ft)", "gauge(mm)": "0.3.5", width: "35", price: "30" },
+    { sno: 43, particulars: "PATT.TRUE.PL", "gauge(mm)": "0.4.0", width: "111", price: "112" },
+    { sno: 44, particulars: "PATT.TRUE.PL", "gauge(mm)": "0.3.5", width: "111", price: "100" },
+    { sno: 45, particulars: "BOTTOM.TRUE.PL", "gauge(mm)": "0.6.0", width: "68", price: "100" },
+    { sno: 46, particulars: "BOTTOM.TRUE.PL", "gauge(mm)": "0.5.0", width: "68", price: "88" },
+    { sno: 47, particulars: "STUD.3.inc.10.FT", "gauge(mm)": "0.5.0", width: "148", price: "173" },
+    { sno: 48, particulars: "FLOOR.3.inc.10.FT", "gauge(mm)": "0.5.0", width: "124", price: "149" },
+    { sno: 49, particulars: "STUD.3.inc.10.FT", "gauge(mm)": "0.4.0", width: "148", price: "147" },
+    { sno: 50, particulars: "FLOOR.3.Inc.10.FT", "gauge(mm)": "0.4.0", width: "124", price: "126" },
+    { sno: 51, particulars: "STUD.2.inc.10.FT", "gauge(mm)": "0.5.0", width: "124", price: "147" },
+    { sno: 52, particulars: "FLOOR.2.inc.10.FT", "gauge(mm)": "0.5.0", width: "101", price: "123" },
+    { sno: 53, particulars: "STUD.2.inc.10.FT", "gauge(mm)": "0.4.0", width: "124", price: "120" },
+    { sno: 54, particulars: "FLOOR.2.inc.10.FT", "gauge(mm)": "0.4.0", width: "101", price: "101" },
+    { sno: 55, particulars: "STUD.3.inc.12.FT", "gauge(mm)": "0.5.0", width: "148", price: "207" },
+    { sno: 56, particulars: "FLOOR.3.inc.12.FT", "gauge(mm)": "0.5.0", width: "124", price: "179" },
+    { sno: 57, particulars: "STUD.3.inc.12.FT", "gauge(mm)": "0.4.0", width: "148", price: "176" },
+    { sno: 58, particulars: "FLOOR.3.Inc.12.FT", "gauge(mm)": "0.4.0", width: "124", price: "152" },
+    { sno: 59, particulars: "STUD.2.inc.12.FT", "gauge(mm)": "0.5.0", width: "124", price: "177" },
+    { sno: 60, particulars: "FLOOR.2.inc.12.FT", "gauge(mm)": "0.5.0", width: "101", price: "148" },
+    { sno: 61, particulars: "STUD.2.inc.12.FT", "gauge(mm)": "0.4.0", width: "124", price: "144" },
+    { sno: 62, particulars: "FLOOR.2.inc.12.FT", "gauge(mm)": "0.4.0", width: "101", price: "121" }
 ];
 
 // DOM elements
@@ -82,7 +82,7 @@ function displayProducts(productsToDisplay) {
         row.innerHTML = `
             <td>${product.sno}</td>
             <td>${product.particulars}</td>
-            <td>${product.gauge}</td>
+            <td>${product["gauge(mm)"]}</td>
             <td>${product.width}</td>
             <td>${product.price}</td>
         `;
@@ -101,7 +101,7 @@ function filterProducts() {
     if (searchTerm) {
         filteredProducts = filteredProducts.filter(product => 
             product.particulars.toLowerCase().includes(searchTerm) ||
-            product.gauge.includes(searchTerm) ||
+            product["gauge(mm)"].toLowerCase().includes(searchTerm) ||
             product.width.includes(searchTerm) ||
             product.price.includes(searchTerm)
         );
@@ -110,7 +110,7 @@ function filterProducts() {
     // Apply category filter
     if (category !== 'all') {
         filteredProducts = filteredProducts.filter(product => 
-            product.particulars.startsWith(category)
+            product.particulars.toLowerCase().includes(category.toLowerCase())
         );
     }
     
